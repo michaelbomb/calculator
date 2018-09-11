29ad310fe8d3b8167afdf780cbc28a36e5f76524
KK <michaelbomb@tutanota.com>
2018-09-11 12:23:05 +0000

    It's working... I guess...

--- main.js
+++ main.js
@@ -35,7 +35,13 @@ function operate(operator, a, b)
     return f(a, b);
 }
 
-
+function evaluateExpression(str)
+{
+    let tmp = eval(str);
+    if (typeof(tmp) == 'number')
+        return tmp;
+    return 'ERROR';
+}
 
 function addEventListeners()
 {
@@ -45,8 +51,25 @@ function addEventListeners()
     bc.addEventListener('click', () => {sc.textContent = "";})
 
     bdel = document.querySelector('#bdel')
-    bdel.addEventListener('click', () => {tmp = sc.textContent; sc.textContent = tmp.slice(0,-1);})
+    bdel.addEventListener('click', () => {
+        sc.textContent = sc.textContent.slice(0,-1);});
     
+    beq = document.querySelector('#beq')
+    beq.addEventListener('click', () => {
+            sc.textContent = evaluateExpression(sc.textContent);})  
+            
+    bts = ['#bdiv','#bmul','#bsub','#badd','#blp','#brp','#bdot'];
+    for (let i = 0; i <= 9; i++)
+        bts.push('#b'+i.toString());
+    
+    for (let i = 0; i < bts.length; i++)
+    {
+        let id = bts[i];
+        let el = document.querySelector(id);
+        el.addEventListener('click',() => {
+            sc.textContent += el.textContent;
+        })
+    }
 }
 
 
